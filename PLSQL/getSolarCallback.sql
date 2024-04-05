ef40f2c8e573ccb9ba0d7e37d8ff3af31b9cf6ea
josh <joshjames.harney@outlook.com>
2024-04-05 17:46:28 +0100

    added functionality, alarms and warnings APEX css

--- PLSQL/getSolarCallback.sql
+++ PLSQL/getSolarCallback.sql
@@ -6,6 +6,11 @@ DECLARE
     l_row     pls_integer := 1;
     l_spoweridx pls_integer;
     l_timeidx pls_integer;
+    l_inverterTempidx pls_integer;
+    l_batterychargeidx pls_integer;
+    l_batterytempidx pls_integer;
+    l_inverterstatus pls_integer;
+
  
 BEGIN
     l_context := apex_exec.open_rest_source_query(
@@ -16,8 +21,11 @@ BEGIN
 
 
     l_spoweridx := apex_exec.get_column_position( l_context, 'SOLAR_POWER' );
+    l_inverterTempidx := apex_exec.get_column_position( l_context, 'INVERTER_TEMPERATURE' );
     l_timeidx := apex_exec.get_column_position( l_context, 'TIME' );
-
+    l_batterychargeidx := apex_exec.get_column_position( l_context, 'BATTERY_CHARGE' );
+    l_batterytempidx := apex_exec.get_column_position( l_context, 'BATTERY_TEMPERATURE' );
+    l_inverterstatus := apex_exec.get_column_position( l_context, 'STATUS' );
 
     while apex_exec.next_row( l_context ) LOOP
 
@@ -26,6 +34,11 @@ BEGIN
     apex_json.write('success', true);  
     apex_json.write('SPOWER', apex_exec.get_number( l_context, l_spoweridx  ));  
     apex_json.write('TIME', apex_exec.get_timestamp_tz( l_context, l_timeidx ));  
+    apex_json.write('INVERTER_TEMPERATURE', apex_exec.get_number( l_context, l_inverterTempidx ));
+    apex_json.write('BATTERY_CHARGE', apex_exec.get_number( l_context, l_batterychargeidx ));
+    apex_json.write('BATTERY_TEMPERATURE', apex_exec.get_number( l_context, l_batterytempidx ));
+    apex_json.write('STATUS', apex_exec.get_varchar2( l_context, l_inverterstatus ));
+
     apex_json.close_object; 
     apex_json.close_all;          
     END LOOP;
--- javascript/currentpowerloop.js
+++ javascript/currentpowerloop.js
@@ -1,30 +1,74 @@
 
-let currentSolar;
+/* const alarm values */
+let intervalID;
+
+const inverterTempWarning = 35;
+const batteryTempWarning = 35;
+const inverterTempAlarm =42;
+const batteryTempAlarm = 30;
+
+const solarindicatorclasses = ["solarindicator"];
 
 function getSolarData() {
-    var solarResult;
+
     let ajaxPromise = apex.server.process("getCurrentSolar");
-    
-    ajaxPromise.done(function (data) { 
-        solarResult = data }
-                                
-        ).fail( function ( jqXHR, textStatus, errorThrown ) 
-        {
+
+    ajaxPromise.done((data) => {
+        console.log("data: ", data);
+        if (data.success) {
+            document.getElementById("solarnow").innerText = `Solar Now:  ${data.SPOWER} W`;
+            document.getElementById("inverterstate").innerText = 'Inverter State: '.concat(data.STATUS);
+            document.getElementById("invertertemperature").innerText = `Inverter Temperature: ${data.INVERTER_TEMPERATURE} °C`;
+            document.getElementById("batterycharge").innerText = `Battery Charge: ${data.BATTERY_CHARGE} %`;
+            document.getElementById("batterytemperature").innerText = `Battery Temperature: ${data.BATTERY_TEMPERATURE} °C`;
+            document.getElementById("batterytemperature").innerText = `Battery Temperature: ${data.BATTERY_TEMPERATURE} °C`;
+            
+            if (data.INVERTER_TEMPERATURE > inverterTempAlarm) {
+                 document.getElementById("invertertemperature").classList.add("u-danger");
+             }
+            else if (data.INVERTER_TEMPERATURE > inverterTempWarning) {
+                document.getElementById("invertertemperature").classList.add("u-warning");
+            }
+            else {
+                document.getElementById("invertertemperature").classList = solarindicatorclasses;
+            }
+
+            if (data.BATTERY_TEMPERATURE > batteryTempAlarm) {
+                document.getElementById("batterytemperature").classList.add("u-danger");
+            }
+            else if (data.BATTERY_TEMPERATURE > batteryTempWarning) {
+                document.getElementById("batterytemperature").classList.add("u-warning");
+            }
+            else {
+                document.getElementById("batterytemperature").classList = solarindicatorclasses;
+            }
+            switch (data.STATUS) {
+                case "Normal":
+                    document.getElementById("inverterstate").classList.add("u-success");
+                    break;
+              
+                case "Unknown":
+                    document.getElementById("inverterstate").classList.add("u-warning");
+                    break;
+
+                default:
+                    document.getElementById("inverterstate").classList = solarindicatorclasses.concat("u-danger");
+            }
+        }
+        else {
+            console.log("Error: ajax callback returned something other than success");
+        }
+
+    })
+        .fail(function (jqXHR, textStatus, errorThrown) {
             console.log("jqXHR: ", jqXHR);
             console.log("textStatus: ", textStatus);
-            console.log("errorThrown: ", errorThrown) });
-
-    return solarResult;
-};
-let logSolar = function logSolar() {
-    let currentSolar = getSolarData();
-    console.log("currentSolar: ", currentSolar);
-
+            console.log("errorThrown: ", errorThrown);
+        });
 };
 
+getSolarData();
 
-logSolar();
-
-setInterval(
-    logSolar, 5 * 60 * 1000); // 5 minutes in milliseconds
+intervalID = setInterval(
+    getSolarData, 5 * 60 * 100); // 5 minutes in milliseconds
 
